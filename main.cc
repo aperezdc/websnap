4c52d423d3b969d2475a49d7bd4df94d51e3698d
Adrian Perez de Castro <aperez@igalia.com>
2013-04-03 00:56:27 +0300

    Set default QWebSettings to reasomable defaults
    Plugins, JavaScript, offline storage, etc, are disabled by
    default.

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
  */
 
 #include <QApplication>
+#include <QWebSettings>
 #include <QWebFrame>
 #include <QWebPage>
 #include <QUrl>
@@ -17,15 +18,21 @@
 int
 main(int argc, char *argv[])
 {
-    QApplication app(argc, argv, QApplication::GuiServer);
-    std::fprintf(stderr, "QApplication created\n");
+    QApplication app(argc, argv);
 
-    WebSnapPage page;
-    std::fprintf(stderr, "Page created\n");
-    QUrl url(QUrl::fromUserInput((argc > 1) ? argv[1] : "http://igalia.com"));
-    std::fprintf(stderr, "Loading %s\n", qPrintable(url.toString()));
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::JavaEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::JavascriptEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::PluginsEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::LocalStorageEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::LocalStorageDatabaseEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::OfflineStorageDatabaseEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::OfflineWebApplicationCacheEnabled, false);
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::FrameFlatteningEnabled, true);
 
+    QWebPage page;
     WebSnap snap(page);
+    QUrl url(QUrl::fromUserInput((argc > 1) ? argv[1] : "http://igalia.com"));
     Perf perf(snap,
               (argc > 2) ? strtoul(argv[2], NULL, 0) : 1,
               (argc > 3) ? QString(argv[3]) : QString::null);
